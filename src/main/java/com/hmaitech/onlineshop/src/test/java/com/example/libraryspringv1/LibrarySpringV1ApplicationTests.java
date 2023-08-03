eb62229ed29d9d7b4490b3f2cb0d682c46420950
YaserJamali <yaser.jamali@outlook.com>
2023-08-03 16:34:38 +0430

    first step yaser jamali branch

--- src/main/java/com/hmaitech/onlineshop/src/test/java/com/example/libraryspringv1/LibrarySpringV1ApplicationTests.java
+++ src/main/java/com/hmaitech/onlineshop/src/test/java/com/example/libraryspringv1/LibrarySpringV1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.libraryspringv1;
+package com.hmaitech.onlineshop.src.test.java.com.example.libraryspringv1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
